8afa63027785f1b003891ec985a6d032066bf022
Igor Stojanovic <igorstojanovic@igors-macbook-air.local>
2024-11-03 11:34:51 +0100

    Enable JWT filter

--- src/main/java/com/memento/tech/backoffice/configuration/BackofficeJwtAuthenticationFilter.java
+++ src/main/java/com/memento/tech/backoffice/configuration/BackofficeJwtAuthenticationFilter.java
@@ -10,12 +10,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
 
-//@Component
+@Component
 public class BackofficeJwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenUtil jwtTokenUtil;
