03b828fde07d813e6e109e21296e43166be1130c
Igor Stojanovic <igorstojanovic@igors-macbook-air.local>
2024-11-03 12:01:48 +0100

    initial frontend

--- src/main/java/com/memento/tech/backoffice/configuration/BackofficeSecurityAutoconfiguration.java
+++ src/main/java/com/memento/tech/backoffice/configuration/BackofficeSecurityAutoconfiguration.java
@@ -28,33 +28,64 @@ public class BackofficeSecurityAutoconfiguration {
     @Value("${memento.tech.backoffice.media.mapping}")
     private String mediaMapping;
 
+//    @Bean
+//    public SecurityFilterChain backofficeSecurity(HttpSecurity http) throws Exception {
+//        return http
+//                .securityMatcher("/backoffice**", "/api/backoffice**", mediaMapping)
+//                .cors(cors -> cors.configurationSource(request -> {
+//                    CorsConfiguration configuration = new CorsConfiguration();
+//                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://localhost:3000"));
+//                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "UPDATE", "OPTIONS"));
+//                    configuration.setAllowedHeaders(List.of("*"));
+//                    configuration.setAllowCredentials(true);
+//                    return configuration;
+//                }))
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(request -> request
+//                        .requestMatchers("/api/auth/**", mediaMapping)
+//                        .permitAll()
+//                        .anyRequest()
+//                        .authenticated())
+//                .sessionManagement(sessionConfigurer -> sessionConfigurer
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+//                .authenticationProvider(backofficeAuthenticationProvider)
+//                .addFilterBefore(backofficeJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                .build();
+//    }
+
     @Bean
     public SecurityFilterChain backofficeSecurity(HttpSecurity http) throws Exception {
         return http
-                .securityMatcher("/backoffice**", "/api/backoffice**", mediaMapping)
+                .securityMatcher("/backoffice/**", "/api/backoffice/**", mediaMapping)
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://localhost:3000"));
-                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "UPDATE", "OPTIONS"));
-                    configuration.setAllowedHeaders(List.of("*"));
-                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow requests from the React app
+                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+                    configuration.setAllowedHeaders(List.of("*")); // Allow all headers
+                    configuration.setAllowCredentials(true); // Allow credentials
                     return configuration;
                 }))
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for stateless session
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/api/auth/**", mediaMapping)
+                        .requestMatchers("/api/auth/**", mediaMapping) // Permit all requests to auth and media mapping
                         .permitAll()
-                        .anyRequest()
+                        .anyRequest() // All other requests require authentication
                         .authenticated())
-                .sessionManagement(sessionConfigurer -> sessionConfigurer
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+                .sessionManagement(sessionConfigurer ->
+                        sessionConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session management
                 .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
+                        .loginPage("/login") // Set custom login page URL
+                        .permitAll() // Allow all users to access the login page
+                        .defaultSuccessUrl("/backoffice", true) // Redirect to /backoffice after successful login
+                        .failureUrl("/login?error=true") // Redirect to login with error if login fails
                 )
                 .authenticationProvider(backofficeAuthenticationProvider)
-                .addFilterBefore(backofficeJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(backofficeJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // Add JWT filter
                 .build();
     }
 }
